57139988928287f2c4ec76cfffae176d9d78465e
ThomasKro <thomas@blue-panda.de>
2022-07-27 11:48:30 +0200

    feat(app): Dropzone in mediathek
    - added a dropzone in mediathek
    - allowed multi file upload via drag-drop

--- packages/antify-admin/src/pages/admin/[tenantId]/media/index.vue
+++ packages/antify-admin/src/pages/admin/[tenantId]/media/index.vue
@@ -5,28 +5,85 @@ import Media from '~~/components/entity/media/Media.vue';
 const file = ref({});
 const { $toaster } = useNuxtApp();
 const route = useRoute();
+const dropzone = ref(null);
+const counter = ref(0);
+const uploading = ref(false);
+
 const { data, refresh: reloadAllMedia } = await useFetch<Response>(
   () => `/api/admin/:tenantId/media?search=${route.query.search || ''}`,
   useDefaultFetchOpts()
 );
 
-const onSelectFile = async (event) => {
+const onSelectFile = async (files: File[]) => {
+  uploading.value = true;
   let formData = new FormData();
 
-  for (let i = 0; i < event.target.files.length; i++) {
-    formData.append(`file-${i}`, event.target.files[i]);
+  for (let i = 0; i < 5; i++) {
+    formData.append(`file-${i}`, files[0]);
+    files.splice(0, 1);
   }
 
-  await useFetch('/api/admin/:tenantId/media', {
+  const result = await useFetch('/api/admin/:tenantId/media', {
     ...useDefaultFetchOpts(),
     method: 'POST',
     body: formData,
   });
 
+  await reloadAllMedia();
+
+  if (files.length > 0) {
+    return await onSelectFile(files);
+  }
+
   $toaster.toastCreated();
 
-  reloadAllMedia();
+  uploading.value = false;
 };
+
+function onDrop(files: File[]) {
+  console.log('dropped', files);
+  onSelectFile(files);
+
+  counter.value = 0;
+
+  dropzone.value.$el.classList.add('hidden');
+}
+
+function dragOverHandler(event: DragEvent) {
+  event.preventDefault();
+
+  dropzone.value.$el.classList.remove('hidden');
+}
+
+function dragEnterHandler(event: DragEvent) {
+  event.preventDefault();
+
+  counter.value++;
+
+  dropzone.value.$el.classList.remove('hidden');
+}
+
+function dragLeaveHandler(event: DragEvent) {
+  event.preventDefault();
+
+  counter.value--;
+
+  if (counter.value === 0) {
+    dropzone.value.$el.classList.add('hidden');
+  }
+}
+
+onMounted(() => {
+  document.body.addEventListener('dragover', dragOverHandler);
+  document.body.addEventListener('dragenter', dragEnterHandler);
+  document.body.addEventListener('dragleave', dragLeaveHandler);
+});
+
+onUnmounted(() => {
+  document.body.removeEventListener('dragover', dragOverHandler);
+  document.body.removeEventListener('dragenter', dragEnterHandler);
+  document.body.removeEventListener('dragleave', dragLeaveHandler);
+});
 </script>
 
 <template>
@@ -34,7 +91,12 @@ const onSelectFile = async (event) => {
     <template #head>
       <AntHeader>Mediatheke</AntHeader>
 
-      <AntUpload v-model:value="file" @change="onSelectFile" :label-style="''">
+      <AntUpload
+        v-model:value="file"
+        @upload="onSelectFile"
+        :label-style="''"
+        :loading="false"
+      >
         <template #preview><span></span></template>
         <template #label>
           <CreateButton label="Hochladen" class="pointer-events-none" />
@@ -43,6 +105,31 @@ const onSelectFile = async (event) => {
     </template>
 
     <template #body>
+      <AntDropzone
+        id="media-dropzone"
+        data-cy="media-dropzone"
+        ref="dropzone"
+        class="hidden absolute h-screen w-screen inset-0 bg-primary bg-opacity-75 z-50 p-4"
+        @dropped="onDrop"
+      >
+        <div
+          class="flex justify-center items-center text-5xl text-white h-full border-4 border-dashed rounded-lg"
+        >
+          Datei hochladen
+        </div>
+      </AntDropzone>
+
+      <div
+        v-if="uploading"
+        class="absolute h-screen w-screen inset-0 bg-black bg-opacity-75 z-50 p-4"
+      >
+        <div
+          class="flex justify-center items-center text-5xl text-white h-full border-4 border-dashed rounded-lg"
+        >
+          Bitte warten Sie bis die Datei(en) Hochgeladen wurden.
+        </div>
+      </div>
+
       <Media :media-files="data.default" @reload-media="reloadAllMedia" />
     </template>
   </AntContent>
--- packages/antify-admin/src/server/api/admin/[tenantId]/media/index.post.ts
+++ packages/antify-admin/src/server/api/admin/[tenantId]/media/index.post.ts
@@ -1,44 +1,56 @@
-import formidable, { File, Files } from "formidable";
+import formidable, { File, Files } from 'formidable';
 import prisma from '~~/server/datasources/db/client';
-import { useGuard } from "~~/composables/useGuard";
+import { useGuard } from '~~/composables/useGuard';
 import { PermissionId } from '~~/server/datasources/static/permissions';
 import { tenantContextMiddleware } from '~~/server/guard/tenantContext.middleware';
 import { useAuthorizationHeader } from '~~/server/utils/useAuthorizationHeader';
 import { useTenantHeader } from '~~/server/utils/useTenantHeader';
-import { HttpForbiddenError } from "~~/server/errors";
-import { useMediaStorage } from "~~/server/service/useMediaService";
+import { HttpForbiddenError } from '~~/server/errors';
+import { useMediaStorage } from '~~/server/service/useMediaService';
 
 export default defineEventHandler(async (event) => {
-    tenantContextMiddleware(event);
+  tenantContextMiddleware(event);
 
-    const guard = useGuard(useAuthorizationHeader(event));
-    const tenantId = useTenantHeader(event);
+  const guard = useGuard(useAuthorizationHeader(event));
+  const tenantId = useTenantHeader(event);
 
-    if (!guard.hasPermissionTo(PermissionId.CAN_CREATE_MEDIA, tenantId)) {
-        throw new HttpForbiddenError();
-    }
+  if (!guard.hasPermissionTo(PermissionId.CAN_CREATE_MEDIA, tenantId)) {
+    throw new HttpForbiddenError();
+  }
 
-    const mediaStorage = useMediaStorage();
-    const form = formidable({ multiples: true, uploadDir: mediaStorage.getAbsoluteUploadDir(), keepExtensions: true });
+  const mediaStorage = useMediaStorage();
+  const form = formidable({
+    multiples: true,
+    uploadDir: mediaStorage.getAbsoluteUploadDir(),
+    keepExtensions: true,
+  });
 
-    // TODO:: limit allowed file types
+  // TODO:: limit allowed file types
 
-    const files: Files = await new Promise((resolve, reject) => {
-        form.parse(event.req, async (err, fields, files) => {
-            // TODO:: check err
-            resolve(files);
-        });
+  const files: Files = await new Promise((resolve, reject) => {
+		// TODO:: if uploading multiple large files (>5MB) this here fails some times
+    form.parse(event.req, async (err, fields, files) => {
+      if (err) {
+        reject('could not parse form');
+        console.log('FEHLER', err);
+      }
+
+      resolve(files);
     });
+  });
 
-    await Promise.all(Object.values(files).map((file: File) => {
-        return prisma.media.create({
-            data: {
-                title: file.originalFilename,
-                fileName: file.newFilename,
-                fileType: file.mimetype
-            }
-        });
-    }));
+  await Promise.all(
+    Object.values(files).map((file: File) => {
+      console.log('Make file: ', file.originalFilename);
+      return prisma.media.create({
+        data: {
+          title: file.originalFilename,
+          fileName: file.newFilename,
+          fileType: file.mimetype,
+        },
+      });
+    })
+  );
 
-    return {};
+  return {};
 });
