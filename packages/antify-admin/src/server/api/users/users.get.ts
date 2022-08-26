f46f70133cb1f6531638644ec63bd60b9ce23dee
ThomasKro <thomas@blue-panda.de>
2022-08-26 10:51:43 +0200

    feat(app): filter users for tenant access.

--- packages/antify-admin/src/server/api/users/users.get.ts
+++ packages/antify-admin/src/server/api/users/users.get.ts
@@ -1,28 +1,44 @@
-import { useGuard } from "~~/composables/useGuard";
+import { useGuard } from '~~/composables/useGuard';
 import { PermissionId } from '~~/server/datasources/static/permissions';
 import { HttpForbiddenError } from '~~/server/errors';
 import { tenantContextMiddleware } from '~~/server/guard/tenantContext.middleware';
 import { useAuthorizationHeader } from '~~/server/utils/useAuthorizationHeader';
 import { useTenantHeader } from '~~/server/utils/useTenantHeader';
-import prisma from "~~/server/datasources/core/client";
+import prisma from '~~/server/datasources/core/client';
 
 export default defineEventHandler(async (event) => {
-    tenantContextMiddleware(event);
+  tenantContextMiddleware(event);
 
-    const guard = useGuard(useAuthorizationHeader(event));
-    const tenantId = useTenantHeader(event);
+  const guard = useGuard(useAuthorizationHeader(event));
+  const tenantId = useTenantHeader(event);
 
-    if (!guard.hasPermissionTo(PermissionId.CAN_READ_USER, tenantId)) {
-        throw new HttpForbiddenError();
-    }
-    
-    const users = await prisma.user.findMany({
-        select: {
-            id: true,
-            email: true,
-            name: true,
-        }
-    });
+  if (!guard.hasPermissionTo(PermissionId.CAN_READ_USER, tenantId)) {
+    throw new HttpForbiddenError();
+  }
 
-    return users;
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      isSuperAdmin: true,
+      tenantAccesses: true,
+    },
+    where: {
+      OR: [
+        {
+          tenantAccesses: {
+            every: {
+              tenantId: tenantId,
+            },
+          },
+        },
+        {
+          isSuperAdmin: true,
+        },
+      ],
+    },
+  });
+
+  return users;
 });
