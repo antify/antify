1889d8a2ef28d6addb6765da3f1fd13a9a3045f4
Johannes Beyer <info@beyer-johannes.de>
2023-02-01 12:21:02 +0100

    feat(toaster): rename to module

---
+++ packages/ant-toaster-module/src/runtime/components/Toaster.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { AntToaster, ANT_TOASTER_TYPE } from '@antify/antify-ui';
+import { ToastType } from '../types';
+const { $toaster } = useNuxtApp();
+
+const toasts = computed(() => {
+  const elem = $toaster.getToasts().map((toast) => ({
+    id: toast.id,
+    message: toast.text,
+    type:
+      toast.type === ToastType.error
+        ? ANT_TOASTER_TYPE.ERROR
+        : toast.type === ToastType.warning
+        ? ANT_TOASTER_TYPE.WARNING
+        : ANT_TOASTER_TYPE.NOTIFICATION,
+  }));
+
+  return elem;
+});
+</script>
+
+<template>
+  <AntToaster :toasts="toasts">
+    <template #default="{ toast }">{{ toast.message }}</template>
+  </AntToaster>
+</template>
